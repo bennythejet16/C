ab272c6d661e38409f9e82a2777f748d237f903b
Benjamin <54950605+bennythejet16@users.noreply.github.com>
2021-03-04 09:52:17 -0800

    Add files via upload

---
+++ helloworld.c
@@ -0,0 +1,10 @@
+// helloworld.c
+// compile : gcc helloworld.c -o helloworld.o
+#include <stdio.h>
+
+int main(){
+	printf("hello world\n");
+	printf("Hola Mundo\n");
+	printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n");
+	return 0;
+}
