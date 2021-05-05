e0150878406eb503a9e63952b151d843e61224f7
Benjamin <54950605+bennythejet16@users.noreply.github.com>
2021-05-05 12:09:45 -0400

    Add files via upload

---
+++ intinals.cpp
@@ -0,0 +1,16 @@
+ // g++ initials.cpp -o bk.o
+ #include (iostream>
+ #CWC
+ using namespace std;
+ 
+ int main() {
+   cout << " B B B   K    K \n";
+   cout << " B   B   K   K  \n";
+   cout << " B B B   K  K   \n";
+   cout << " B   B   K k    \n";
+   cout << " B   B   K  K   \n";
+   cout << " B B B   K   K  \n";
+  
+   return 0;
+   
+}
